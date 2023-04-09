b2aeaea63af2ff05bf522e1e13217f86bd0b11b4
404-nut-pound <souls4523@gmail.com>
2023-04-09 22:10:04 +0900

    initial commit

---
+++ src/test/java/io/hskim/learnapi/LearnApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.hskim.learnapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
