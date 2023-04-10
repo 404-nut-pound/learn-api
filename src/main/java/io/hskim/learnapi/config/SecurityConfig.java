3afdcd4c964814f6a6436020e2f8fe4e0f6769f9
404-nut-pound <souls4523@gmail.com>
2023-04-11 02:24:02 +0900

    [feat] Config를 통해 유저를 등록하고 yml에서 유저 정보 삭제

--- src/main/java/io/hskim/learnapi/config/SecurityConfig.java
+++ src/main/java/io/hskim/learnapi/config/SecurityConfig.java
@@ -4,6 +4,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -17,13 +22,30 @@ public class SecurityConfig {
   public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity)
     throws Exception {
     return httpSecurity
-      .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+      .authorizeHttpRequests(request -> request.anyRequest().authenticated())
       .build();
   }
 
+  // 해당 방법 보다는 SecurityFilterChain의 permitAll을 통해서 사용하기를 권장
   @Bean
   public WebSecurityCustomizer webSecurityCustomizer() {
-    // 해당 방법 보다는 SecurityFilterChain의 permitAll을 통해서 사용하기를 권장
     return web -> web.ignoring().antMatchers("/ignore1", "/ignore2");
+  }
+
+  @Bean
+  public InMemoryUserDetailsManager userDetailsManager() {
+    UserDetails user = User
+      .builder()
+      .username("user")
+      .password("passw0rd")
+      .roles("USER")
+      .build();
+
+    return new InMemoryUserDetailsManager(user);
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
   }
 }
