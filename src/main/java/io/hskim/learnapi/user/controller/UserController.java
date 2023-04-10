f88c5ee3138c9bc2585866ffd4a1dceb6c148213
404-nut-pound <souls4523@gmail.com>
2023-04-11 00:16:56 +0900

    [feat] HATEOAS 추가

--- src/main/java/io/hskim/learnapi/user/controller/UserController.java
+++ src/main/java/io/hskim/learnapi/user/controller/UserController.java
@@ -3,6 +3,8 @@ package io.hskim.learnapi.user.controller;
 import io.hskim.learnapi.user.dto.UserDto;
 import io.hskim.learnapi.user.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -37,7 +39,17 @@ public class UserController {
   // @GetMapping(path = "/{id}", params = { "version=1" }) //query string에 추가
   // @GetMapping(path = "/{id}", headers = { "X-API-VERSION=1" }) //header에 추가
   // @GetMapping(path = "/{id}", produces = { "application/vnd.company.appv1+json" }) //header accept에 추가
-  public ResponseEntity<?> getUser(@PathVariable int id) {
-    return ResponseEntity.ok().body(userService.getUser(id));
+  public EntityModel<?> getUser(@PathVariable int id) {
+    UserDto user = userService.getUser(id);
+
+    return EntityModel.of(
+      user,
+      WebMvcLinkBuilder
+        .linkTo(WebMvcLinkBuilder.methodOn(UserController.class).getUser(id))
+        .withSelfRel(),
+      WebMvcLinkBuilder
+        .linkTo(WebMvcLinkBuilder.methodOn(UserController.class).getUserList())
+        .withRel("user-list")
+    );
   }
 }
