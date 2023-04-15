36a2b9124987d0839d4d1534afa5072a31114e56
404-nut-pound <souls4523@gmail.com>
2023-04-15 16:35:55 +0900

    [feat] Spring Security 관련 설정 추가
    - 비밀번호 인코더 Bean 추가
    - UserDetailService Bean 추가
    - HttpSecurity 설정 추가

--- src/main/java/io/hskim/learnapi/config/SecurityConfig.java
+++ src/main/java/io/hskim/learnapi/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -18,11 +19,53 @@ import org.springframework.security.web.SecurityFilterChain;
  */
 public class SecurityConfig {
 
+  private static BCryptPasswordEncoder encoder;
+
+  @Bean
+  public static BCryptPasswordEncoder getEncoder() {
+    if (encoder == null) {
+      encoder = new BCryptPasswordEncoder();
+    }
+
+    return encoder;
+  }
+
+  // private final UserService userService; //UserDetailService 구현 클래스 지정
+
+  /**
+   * 참조 링크
+   * https://hou27.tistory.com/entry/Spring-Security-%EC%84%B8%EC%85%98-%EC%9D%B8%EC%A6%9D
+   * @return
+   */
+  // @Bean
+  // public UserDetailsService userDetailsService() {
+  //   return userService();
+  // }
+
+  /**
+   * Session 참조 링크
+   * https://ugo04.tistory.com/167
+   * @param httpSecurity
+   * @return
+   * @throws Exception
+   */
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity)
     throws Exception {
     return httpSecurity
-      .authorizeHttpRequests(request -> request.anyRequest().authenticated())
+      .authorizeHttpRequests(request -> request.anyRequest().permitAll())
+      .sessionManagement(management ->
+        management
+          .sessionCreationPolicy(SessionCreationPolicy.STATELESS) //stateless 설정
+          .sessionFixation(sf -> sf.changeSessionId()) //세션 생성 방식, 매번 새로운 세션 생성
+          .maximumSessions(1) //동일한 접속 정보에 대해 최대 세션 수
+          .maxSessionsPreventsLogin(true) //최대 세션 수를 초과했을 때 로그인 허용 여부
+          .expiredUrl("/auth") //세션 만료 시 redirect url
+      )
+      .csrf(csrf -> csrf.disable()) //stateless 설정에 따라 csrf off
+      .headers(header -> header.frameOptions().disable()) //헤더를 통한 frame option off
+      .formLogin(formLogin -> formLogin.disable()) //별도의 front-end를 사용하기 때문에 로그인 페이지 비활성화
+      .logout(logout -> logout.disable())
       .build();
   }
 
